a3374b8b6d67dd0478ea63d9c64ed03c7e98b2c4
shivam <shivam.g@taltrip.com>
2024-05-13 17:53:37 +0530

    creating restraint dashboard page and authenticate that without signup no one enter in dashboard

--- src/app/_components/RestaurantHeader.js
+++ src/app/_components/RestaurantHeader.js
@@ -1,5 +1,26 @@
+"use client";
 import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
 const RestaurantHeader = () => {
+  const [details, setDetails] = useState("");
+  const router = useRouter();
+  const pathName = usePathname();
+  useEffect(() => {
+    let data = localStorage.getItem("restaurantUser");
+    if (!data && pathName == "/restaurant/dashboard") {
+      router.push("/restaurant");
+    } else if (data && pathName == "/restaurant") {
+      router.push("/restaurant/dashboard");
+    } else {
+      setDetails(JSON.parse(data));
+    }
+  }, []);
+  const logout = () => {
+    router.push("/restaurant");
+    localStorage.removeItem("restaurantUser");
+  };
   return (
     <>
       <div className="header-wrapper">
@@ -14,12 +35,20 @@ const RestaurantHeader = () => {
           <li>
             <Link href="/">Home</Link>
           </li>
-          <li>
-            <Link href="/">Login/SignUp</Link>
-          </li>
-          <li>
-            <Link href="/">Profile</Link>
-          </li>
+          {details && details.name ? (
+            <>
+              <li>
+                <button onClick={logout}>Logout</button>
+              </li>
+              <li>
+                <Link href="/">Profile</Link>
+              </li>
+            </>
+          ) : (
+            <li>
+              <Link href="/restaurant">Login/SignUp</Link>
+            </li>
+          )}
         </ul>
       </div>
     </>
---
+++ src/app/restaurant/dashboard/page.js
@@ -0,0 +1,12 @@
+import RestaurantHeader from "@/app/_components/RestaurantHeader";
+import "../style.css";
+const Dashboard = () => {
+  return (
+    <>
+      <RestaurantHeader />
+      <div>Welcome to Dashboard</div>
+    </>
+  );
+};
+
+export default Dashboard;
