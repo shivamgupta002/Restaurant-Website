a6ba9985c2ef7e4019d99cb896105767a9da794c
shivamgupta02 <shivam.g@taltrip.com>
2024-06-07 12:39:50 +0530

    get food by id

--- src/app/api/restaurant/foods/[id]/route.js
+++ src/app/api/restaurant/foods/[id]/route.js
@@ -8,8 +8,9 @@ export async function GET(request, content) {
   let success = false;
   await mongoose.connect(connectionStr, { useNewUrlParser: true });
   //   let resto_id=localStorage.getItem()
-  //   const result = await foodSchema.find({ resto_id: "665598fcdaf549bf1d6e5585" });
-  const result = await foodSchema.find();
+    const result = await foodSchema.find({ resto_id: id });
+  // it gives all food
+  // const result = await foodSchema.find();
   if (result) {
     success = true;
   }
