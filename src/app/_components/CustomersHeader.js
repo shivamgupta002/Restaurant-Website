cd60ac831ff72e621925e2e8b4bb64e25a6295f7
shivamgupta02 <shivam.g@taltrip.com>
2024-06-18 17:37:45 +0530

    Customer Header added

---
+++ src/app/_components/CustomersHeader.js
@@ -0,0 +1,35 @@
+import Link from "next/link";
+
+const CustomerHeader = () => {
+  return (
+    <>
+      <div className="header-wrapper">
+        <div className="logo">
+          <img
+            src="https://png.pngtree.com/template/20200610/ourmid/pngtree-food-delivery-logo-design-image_381319.jpg"
+            alt="logo"
+            style={{ width: 100 }}
+          />
+        </div>
+        <ul>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/">Login</Link>
+          </li>
+          <li>
+            <Link href="/">SignUp</Link>
+          </li>
+          <li>
+            <Link href="/">Cart(0)</Link>
+          </li>
+          <li>
+            <Link href="/">Add Restaurant</Link>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+};
+export default CustomerHeader;
--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,11 @@
-
+import CustomerHeader from "./_components/CustomersHeader";
+import Footer from "./_components/Footer";
 export default function Home() {
   return (
     <main>
+      <CustomerHeader/>
      <h1>Delivery app</h1>
+     <Footer/>
     </main>
   );
 }
