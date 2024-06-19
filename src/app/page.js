875a007d77533f6ce4eabb3cefe7224f93e4ce46
shivamgupta02 <shivam.g@taltrip.com>
2024-06-20 01:58:51 +0530

    all restaurant see on frontend

--- src/app/page.js
+++ src/app/page.js
@@ -6,8 +6,11 @@ export default function Home() {
   const [locations, setLocations] = useState([]);
   const [selectedLocation, setSelectedLocation] = useState("");
   const [showLocation, setShowLocation] = useState(false);
+  const [restaurants, setRestaurants] = useState([]);
+
   useEffect(() => {
     loadLocation();
+    loadRestaurant();
   }, []);
   const loadLocation = async () => {
     let response = await fetch("http://localhost:3000/api/customer/locations");
@@ -21,7 +24,13 @@ export default function Home() {
     setSelectedLocation(item);
     setShowLocation(false);
   };
-
+  const loadRestaurant = async () => {
+    let response = await fetch("http://localhost:3000/api/customer");
+    response = await response.json();
+    if (response.success) {
+      setRestaurants(response.result);
+    }
+  };
   return (
     <main>
       <CustomerHeader />
@@ -47,6 +56,21 @@ export default function Home() {
             placeholder="Enter food or restaurant name"
           />
         </div>
+      </div>
+      <div className="restaurant-list-container">
+        {restaurants.map((item) => (
+          <div className="restaurant-wrapper">
+            <div className="heading-wrapper">
+              <h3>{item.name}</h3>
+              <h5>{item.contact}</h5>
+            </div>
+            <div className="address-wrapper">
+              <div>{item.city},</div>
+              <div className="address">{item.address},</div>
+              <div>{item.email}</div>
+            </div>
+          </div>
+        ))}
       </div>
       <Footer />
     </main>
