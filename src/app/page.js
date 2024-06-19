e3d8c32273342f8491dc5272cea7abb0766234b8
shivamgupta02 <shivam.g@taltrip.com>
2024-06-19 15:29:25 +0530

    added banner and static search box

--- src/app/page.js
+++ src/app/page.js
@@ -3,9 +3,23 @@ import Footer from "./_components/Footer";
 export default function Home() {
   return (
     <main>
-      <CustomerHeader/>
-     <h1>Delivery app</h1>
-     <Footer/>
+      <CustomerHeader />
+      <div className="main-page-banner">
+        <h1>Food Delivery app</h1>
+        <div className="input-wrapper">
+          <input
+            type="text"
+            className="select-input"
+            placeholder="Select Place"
+          />
+          <input
+            type="text"
+            className="search-input"
+            placeholder="Enter food or restaurant name"
+          />
+        </div>
+      </div>
+      <Footer />
     </main>
   );
 }
