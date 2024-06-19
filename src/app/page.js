859c6097685a313ece9bd6fc93b10c20d2194ad1
shivamgupta02 <shivam.g@taltrip.com>
2024-06-19 19:18:41 +0530

    showing location on u

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,27 @@
+"use client";
+import { useEffect, useState } from "react";
 import CustomerHeader from "./_components/CustomersHeader";
 import Footer from "./_components/Footer";
 export default function Home() {
+  const [locations, setLocations] = useState([]);
+  const [selectedLocation, setSelectedLocation] = useState("");
+  const [showLocation, setShowLocation] = useState(false);
+  useEffect(() => {
+    loadLocation();
+  }, []);
+  const loadLocation = async () => {
+    let response = await fetch("http://localhost:3000/api/customer/locations");
+    response = await response.json();
+    if (response.success) {
+      setLocations(response.result);
+    } else {
+    }
+  };
+  const handleListItem = (item) => {
+    setSelectedLocation(item);
+    setShowLocation(false);
+  };
+
   return (
     <main>
       <CustomerHeader />
@@ -11,7 +32,15 @@ export default function Home() {
             type="text"
             className="select-input"
             placeholder="Select Place"
+            value={selectedLocation}
+            onClick={() => setShowLocation(true)}
           />
+          <ul className="location-list">
+            {showLocation &&
+              locations.map((item) => (
+                <li onClick={() => handleListItem(item)}>{item}</li>
+              ))}
+          </ul>
           <input
             type="text"
             className="search-input"
