362e94dcc692e9f5ad34472c5686206414bff18f
shivamgupta02 <shivam.g@taltrip.com>
2024-06-19 16:02:43 +0530

    create api for get location

---
+++ src/app/api/customer/locations/route.js
@@ -0,0 +1,12 @@
+import { connectionStr } from "@/app/lib/db";
+import { restaurantSchema } from "@/app/lib/restaurantModel";
+import mongoose from "mongoose";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  await mongoose.connect(connectionStr, { useNewUrlParser: true });
+  let result =await restaurantSchema.find();
+  result = result.map((item) => item.city.charAt(0).toUpperCase()+item.city.slice(1)); //   {"success":true,"result":["Khairthal","Khairthal","Khairthal","Khairthal"]}
+  result = [...new Set(result.map((item) => item))]; //{"success":true,"result":["Khairthal"]}
+  return NextResponse.json({ success: true, result });
+}
