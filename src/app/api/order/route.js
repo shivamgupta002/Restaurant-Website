a868313785172ba81c753247fe0b48ee7890da55
shivamgupta02 <shivam.g@taltrip.com>
2024-07-05 13:25:20 +0530

    Get my order API ->(Make get api method,Get id from url,from order collection,Get data from food collection )

--- src/app/api/order/route.js
+++ src/app/api/order/route.js
@@ -1,5 +1,6 @@
 import { connectionStr } from "@/app/lib/db";
 import { orderSchema } from "@/app/lib/ordersModel";
+import { restaurantSchema } from "@/app/lib/restaurantModel";
 import mongoose from "mongoose";
 import { NextResponse } from "next/server";
 
@@ -10,6 +11,28 @@ export async function POST(request) {
   const orderObj = new orderSchema(payload);
   const result = await orderObj.save();
   if (result) {
+    success = true;
+  }
+  return NextResponse.json({ result, success });
+}
+
+export async function GET(request) {
+  let success = false;
+  const userId = request.nextUrl.searchParams.get("id");
+  await mongoose.connect(connectionStr, { useNewUrlParser: true });
+  let result = await orderSchema.find({ user_id: userId });
+  if (result) {
+    let restoData = await Promise.all(
+      result.map(async (item) => {
+        let restoInfo = {};
+        restoInfo.data = await restaurantSchema.findOne({ _id: item.resto_id });
+        restoInfo.amount=item.amount;
+        restoInfo.status=item.status;
+        
+        return restoInfo;
+      })
+    );
+    result=restoData;
     success = true;
   }
   return NextResponse.json({ result, success });
