9a37ff394124129c131f5fafffc59aa367d62c13
shivamgupta02 <shivam.g@taltrip.com>
2024-07-06 17:21:19 +0530

    loading component added

---
+++ src/app/loading.js
@@ -0,0 +1,19 @@
+const Loading = () => {
+  return (
+    <>
+      <div style={{ height: "100vh", width: "100vw" }}>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            marginTop:'20%'
+          }}
+        >
+          <h4>Loading...</h4>
+        </div>
+      </div>
+    </>
+  );
+};
+export default Loading;
