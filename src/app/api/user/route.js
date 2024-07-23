e9df853fe303090e9ad3bfaa964e1f62c1b8baa2
shivamgupta02 <shivam.g@taltrip.com>
2024-07-23 17:16:37 +0530

    password hassing

--- src/app/api/user/route.js
+++ src/app/api/user/route.js
@@ -2,15 +2,41 @@ import { connectionStr } from "@/app/lib/db";
 import { userSchema } from "@/app/lib/userModal";
 import mongoose from "mongoose";
 import { NextResponse } from "next/server";
+import bcryptjs from "bcryptjs";
 
 export async function POST(request) {
-  let success = false;
-  const payload = await request.json();
-  await mongoose.connect(connectionStr, { useNewUrlParser: true });
-  const user = new userSchema(payload);
-  const result = await user.save();
-  if (result) {
-    success = true;
+  try {
+    let success = false;
+    await mongoose.connect(connectionStr, { useNewUrlParser: true });
+
+    const reqBody = await request.json();
+    const { name, email, password, city, address, mobile } = reqBody;
+    console.log(name, email, password, city, address, mobile);
+
+    const user =await userSchema.findOne({ email });
+    if (user) {
+      return NextResponse.json({
+        message: "User already exists",
+        success: false,
+      });
+    }
+    const salt =await bcryptjs.genSalt(10);
+    const hashedPassword =await bcryptjs.hash(password, salt);
+    const newUser = new userSchema({
+      name,
+      email,
+      password: hashedPassword,
+      city,
+      address,
+      mobile,
+    });
+    const result = await newUser.save();
+    console.log(result);
+    if (result) {
+      success = true;
+    }
+    return NextResponse.json({ result, success });
+  } catch (error) {
+    console.log("Error in creating signup user: " + error.message);
   }
-  return NextResponse.json({ result, success });
 }
