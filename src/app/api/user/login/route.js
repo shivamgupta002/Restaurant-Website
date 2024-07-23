775575b7a98828fb3541c238d953d379818f8f73
shivamgupta02 <shivam.g@taltrip.com>
2024-07-23 18:00:38 +0530

    login password hashing

--- src/app/api/user/login/route.js
+++ src/app/api/user/login/route.js
@@ -2,17 +2,33 @@ import { connectionStr } from "@/app/lib/db";
 import { userSchema } from "@/app/lib/userModal";
 import mongoose from "mongoose";
 import { NextResponse } from "next/server";
+import bcryptjs from "bcryptjs"
 
 export async function POST(request) {
-  const payload = await request.json();
   let success = false;
-  await mongoose.connect(connectionStr, { useNewUrlParser: true });
-  const result = await userSchema.findOne({
-    email: payload.email,
-    password: payload.password,
-  });
-  if (result) {
-    success = true;
+  try {
+    await mongoose.connect(connectionStr, { useNewUrlParser: true });
+
+    const reqBody = await request.json();
+    const { email, password } = reqBody;
+
+    const user = await userSchema.findOne({ email });
+    if (!user) {
+      return NextResponse.json({ error: "Email Not found" }, { status: 400 });
+    }
+    const validPassword = bcryptjs.compare(password, user.password);
+    if (!validPassword) {
+      return NextResponse.json(
+        { error: "Check your credentials" },
+        { status: 400 }
+      );
+    }
+
+    if (user) {
+      success = true;
+    }
+    return NextResponse.json({ user, success });
+  } catch (error) {
+    console.log("Error while login " + error.message);
   }
-  return NextResponse.json({ result, success });
 }
